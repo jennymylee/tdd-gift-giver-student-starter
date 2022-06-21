0a84228cbe87d937b9cce2a03074e1df8722606b
jennymylee <jennylee038@gmail.com>
2022-06-21 16:02:15 -0700

    35/38

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-module.exports = app;
 
 const morgan = require("morgan");
 const router = require("./routes/gift-exchange");
@@ -11,13 +10,13 @@ app.use(express.json());
 
 app.use("/gift-exchange", router);
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   res.status(200).json({ ping: "pong" });
 });
 
-app.get("gift-exchange", (req, res) => {
-  res.status(200).json(res.body);
-});
+// app.get("gift-exchange", (req, res) => {
+//   res.status(200).json(res.body);
+// });
 
 app.use((req, res, next) => {
   return next(new NotFoundError());
@@ -26,7 +25,7 @@ app.use((req, res, next) => {
 app.use((error, req, res, next) => {
   let status = error.status || 500;
   let message = error.message || "Something went wrong in the application";
-  return res
-    .status(status)
-    .json({ error: { status: { status }, message: { message } } });
+  return res.status(status).json({ error: { status, message } });
 });
+
+module.exports = app;
